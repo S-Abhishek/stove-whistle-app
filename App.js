728290254b7e764dd38a0bb506905949074a4d0d
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2019-02-13 19:36:01 +0530

    Splash screen added

--- App.js
+++ App.js
@@ -11,40 +11,57 @@ import { getStatusBarHeight } from 'react-native-status-bar-height';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true };
+    this.state = { splash_sc : false };
   }
 
-  async componentWillMount() {
-    await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-    });
-    this.setState({ loading: false });
+  componentDidMount()
+  {
+    setTimeout(() => {
+      this.done();
+    },1000);
   }
+
+  done()
+  {
+    this.setState({ splash_sc : true });
+  }
+
+
+
   render() {
-    return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="arrow-back" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Whistle</Title>
-          </Body>
-          <Right>
-            <Button transparent>
-              <Icon name="more" />
-            </Button>
-          </Right>
-        </Header>
-        <Content>
-          
-          
-        </Content>
-      </Container>
-    );
+
+    if(!this.state.splash_sc)
+    {
+      return <SplashScreen />;
+
+    }
+    else 
+    {
+      return (
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent>
+                <Icon name="arrow-back" />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Whistle</Title>
+            </Body>
+            <Right>
+              <Button transparent>
+                <Icon name="more" />
+              </Button>
+            </Right>
+          </Header>
+          <Content>
+            
+            
+          </Content>
+        </Container>
+      );
+    }
+    
   }
 }
 
