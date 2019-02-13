2c4c856938fcaba6eff77edb38b1c05474f067ac
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2019-02-13 20:11:41 +0530

    Added Card for number of whistles

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View , Image } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right , Title} from 'native-base';
 
+var cooker = require('./assets/whistle4.png')
 
 //To prevent overlap with notification bar
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-
 
 
 export default class App extends React.Component {
@@ -32,7 +32,15 @@ export default class App extends React.Component {
 
     if(!this.state.splash_sc)
     {
-      return <SplashScreen />;
+      return (
+
+        <Container>
+          <Title>
+            Hello
+          </Title>
+        </Container>
+
+      );
 
     }
     else 
@@ -55,7 +63,21 @@ export default class App extends React.Component {
             </Right>
           </Header>
           <Content>
-            
+            <Card>
+              <CardItem>
+                <Left>
+                  <Body>
+                    <Text>Number of whistles</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem cardBody>
+                <View style={styles.image}>
+                  <Image source = {cooker} />
+                </View>
+
+              </CardItem>
+            </Card>
             
           </Content>
         </Container>
@@ -73,4 +95,8 @@ const styles = StyleSheet.create({
     paddingTop: getStatusBarHeight(),
     height: 54 + getStatusBarHeight(),
   },
+  image: {
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
