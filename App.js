99c3ac91709cf14aabb15dd3fda46dc26a786a11
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2019-02-13 19:21:08 +0530

    Fixed header and notification overlap and basic layout

--- App.js
+++ App.js
@@ -1,31 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right , Title} from 'native-base';
+
+
+//To prevent overlap with notification bar
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
   render() {
     return (
       <Container>
         <Header>
           <Left>
-
+            <Button transparent>
+              <Icon name="arrow-back" />
+            </Button>
           </Left>
           <Body>
             <Title>Whistle</Title>
           </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="more" />
+            </Button>
+          </Right>
         </Header>
+        <Content>
+          
+          
+        </Content>
       </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  Header: {
+    paddingTop: getStatusBarHeight(),
+    height: 54 + getStatusBarHeight(),
   },
 });
