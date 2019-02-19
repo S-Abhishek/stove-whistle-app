090acd342265a56cef59e5ba8989a4548de1d3d4
hailK11 <akhilkrishna.r2016@vitstudent.ac.in>
2019-02-19 21:24:06 +0530

    Added animation,footer to pop-up

--- Home.js
+++ Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View , Image } from 'react-native';
 import { Container, Header, Content, Accordion , Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right , Title , Footer, FooterTab, Fab} from 'native-base';
-import Dialog, { DialogContent } from 'react-native-popup-dialog';
+import Dialog, { DialogContent, DialogTitle, SlideAnimation, DialogFooter, DialogButton } from 'react-native-popup-dialog';
 
 const dataArray = [
   { title: "Module #1", content: "Testing" },
@@ -13,6 +13,7 @@ var cooker = require('./assets/whistle4.png')
 //To prevent overlap with notification bar
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
+state = { DialogState: false};
 
 export default class Home extends React.Component {
 
@@ -84,13 +85,21 @@ export default class Home extends React.Component {
           <View style={{flex:1}}>
           <Content padder>
           <Accordion dataArray={dataArray} expanded={0}/>
-          <Button block onPress={() => {this.setState({ visible: true });}}>
+          <Button block onPress={() => {this.setState({ DialogState: true });}}>
                 <Text>Configure</Text>
           </Button>            
 
           <Dialog
-            visible={this.state.visible}
-            onTouchOutside={() => {this.setState({ visible: false });}}>
+            DialogState={this.state.DialogState}
+            dialogTitle={<DialogTitle title="Configure Cooker" />}>
+            onTouchOutside={() => {this.setState({ DialogState: false });}}>
+            dialogAnimation={new SlideAnimation({slideFrom: 'top'})}
+            footer={
+              <DialogFooter>
+                <DialogButton text="Cancel" onPress={() => {}}/>
+                <DialogButton text="Submit" onPress={() => {}}/>
+              </DialogFooter>
+            }
             <DialogContent style={{backgroundColor: '#F7F7F8',}}>
               <Text>Customise content</Text>
             </DialogContent>
